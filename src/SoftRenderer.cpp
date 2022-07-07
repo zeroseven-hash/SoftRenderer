7c0701dcb7593075a1e3c42a25790366f8f396d8
zhaoqingfeng <1837631136@qq.com>
2022-07-07 20:16:32 +0800

    update

--- src/SoftRenderer.cpp
+++ src/SoftRenderer.cpp
@@ -7,24 +7,23 @@
 #include"tinyddsloader.h"
 int main()
 {
-    //ThreadPool::Init(std::thread::hardware_concurrency());
-    ThreadPool::Init(1);
+    ThreadPool::Init(std::thread::hardware_concurrency());
 
     Application* app = Application::CreateApp("SoftRenderer", 980, 720);
 
   
-    //shared_ptr<BlinnPhone> blinn_phone = std::make_shared<BlinnPhone>("Blinn-Phone");
-    //blinn_phone->Init();
-    //app->AddScene(blinn_phone);
+    shared_ptr<BlinnPhone> blinn_phone = std::make_shared<BlinnPhone>("Blinn-Phone");
+    blinn_phone->Init();
+    app->AddScene(blinn_phone);
 
-    //shared_ptr<SkeletalAnimation> skeletal_animation = std::make_shared<SkeletalAnimation>("SkeletalAnimation");
-    //skeletal_animation->Init();
-    //app->AddScene(skeletal_animation);
+    shared_ptr<SkeletalAnimation> skeletal_animation = std::make_shared<SkeletalAnimation>("SkeletalAnimation");
+    skeletal_animation->Init();
+    app->AddScene(skeletal_animation);
 
     //
-    //shared_ptr<Pbr> pbr = std::make_shared<Pbr>("Pbr");
-    //pbr->Init();
-    //app->AddScene(pbr);
+    shared_ptr<Pbr> pbr = std::make_shared<Pbr>("Pbr");
+    pbr->Init();
+    app->AddScene(pbr);
 
 
     std::shared_ptr<Ibl> ibl = std::make_shared<Ibl>("Ibl");
